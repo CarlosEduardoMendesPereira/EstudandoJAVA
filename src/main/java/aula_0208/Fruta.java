c5091d21e3db4eccd20e3b47b5d6f8b5b87eb803
CarlosEduardoMendesPereira <carloseduardomendes201@gmail.com>
2023-08-03 08:21:53 -0300

    EstudoClasses

--- src/main/java/aula_0208/Fruta.java
+++ src/main/java/aula_0208/Fruta.java
@@ -1,4 +1,26 @@
 package aula_0208;
 
+// POO (Programação Orientada a Objetos)
+//  Classes -> Fruta (Modelagem)
+//  Objetos -> Maçã, Pera, Banana, Goiaba
+//  Instãncia -> Processo que constrói um objeto a partir de uma classe
+//  Atributo/Propriedade -> Um valor definido na classe incorporado no objeto
+//  Métodos/Funções -> Uma rotina/procedimento/tarefa que é feito pelo objeto
 public class Fruta {
+
+    String nome;
+
+    String cor;
+
+    float acidez;
+
+    boolean suculencia = false;
+
+    boolean maduro = false;
+
+    String[] vitaminas;
+
+    void amadurecer(int dias) {
+        maduro = true;
+    }
 }
